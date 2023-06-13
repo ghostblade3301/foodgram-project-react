fa9623a28360e8cf4ea4221be566e849b089ae1e
Pavel Naumenko <pavelnaumenkowork@gmail.com>
2023-06-13 16:06:29 +0300

    dockerition almost done

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,5 +1,5 @@
 import os
-
+##########
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
 
@@ -117,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -7,6 +7,4 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/docs/', TemplateView.as_view(template_name='redoc.html'),
-         name='redoc',),
 ]
