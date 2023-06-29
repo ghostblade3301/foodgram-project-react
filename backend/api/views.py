7330d200bc283f80fa7d97a816c508e3bbd5dde2
Pavel Naumenko <ghostblade3301@gmail.com>
2023-06-29 03:26:56 +0300

    for tests

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
